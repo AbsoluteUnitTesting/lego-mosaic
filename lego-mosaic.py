fbce3a13bd7d2b8b415e513d47e359dad241479a
Wafflenaut <moyerjo@oregonstate.edu>
2018-11-12 21:13:35 -0700

    Added function to find closest color and began compiling color dictionary with data

--- lego-mosaic.py
+++ lego-mosaic.py
@@ -1,5 +1,22 @@
 import sys, random, argparse, math
 from PIL import Image
+from legoColors import lColors
+
+def closestColor(tileRGB):
+    minDifference = 1000 #255*3 is the max difference between two colors
+    colorMatch = 0
+
+    for key,val in lColors.items():
+        #color weighting used
+        colorDifference = math.sqrt((((val[0]-tileRGB[0]) * .3)**2) + ((val[1]-tileRGB[1]) * .59)**2) + ((val[2]-tileRGB[2]) * .11)**2
+        if(colorDifference < minDifference):
+            minDifference = colorDifference
+            colorMatch = key
+
+    return key
+
+
+
 #hard code in the columns for now - should be entered by user
 cols = 50
 
@@ -8,7 +25,9 @@ sourceImage = Image.open("test-image.jpg")
 imageW, imageH = sourceImage.size[0], sourceImage.size[1]
 print("Image height: " + str(imageH))
 
+#tiles are square (lego pieces)
 tileW = tileH = imageW/cols
+
 #determine how many vertical tiles can fit in image
 croppedH = math.floor(imageH/tileH) * tileH
 print("Cropped height: " + str(croppedH))
@@ -22,4 +41,17 @@ if croppedH != imageH:
 else:
     baseImage = sourceImage
 
+
+#TODO
+#Create 2d Array of color keys
+#Create dictionary of lego id color to lego brick code
+#Create a count of each color (number of pieces needed) and tie to brick code
+#Create image from a 2d array of lego color ids
+#Create a manual for creating lego color ids?
+
+print(lColors[1][0])
+
+
 baseImage.show()
+
+
